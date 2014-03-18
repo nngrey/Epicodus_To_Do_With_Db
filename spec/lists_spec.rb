ab93809e06f42c9cf27c5db3ee755b690a7d6e2e
Nathan Grey & Mark Lehman <student@epicodus.com>
2014-03-17 17:22:11 -0700

    add method to return all tasks for a given list

--- spec/lists_spec.rb
+++ spec/lists_spec.rb
@@ -5,18 +5,18 @@ require 'spec_helper'
 
 describe List do
   it 'is initialized with a name' do
-    list = List.new('Epicodus stuff', 2)
+    list = List.new('Epicodus stuff')
     list.should be_an_instance_of List
   end
 
   it 'tells you its name' do
-    list = List.new('Epicodus stuff', 2)
+    list = List.new('Epicodus stuff')
     list.name.should eq 'Epicodus stuff'
   end
 
   it 'is the same list if it has the same name' do
-    list1 = List.new('Epicodus stuff', 1)
-    list2 = List.new('Epicodus stuff', 1)
+    list1 = List.new('Epicodus stuff')
+    list2 = List.new('Epicodus stuff')
     list1.should eq list2
   end
 
@@ -25,34 +25,36 @@ describe List do
   end
 
   it 'lets you save lists to the database' do
-    list = List.new('learn SQL', 2)
+    list = List.new('learn SQL')
     list.save
     List.all.should eq [list]
   end
 
   it 'sets its ID when you save it' do
-    list = List.new('learn SQL', 2)
+    list = List.new('learn SQL')
     list.save
     list.id.should be_an_instance_of Fixnum
   end
 
   it 'can be initialized with its database ID' do
-    list = List.new('Epicodus stuff', 1)
+    list = List.new('Epicodus stuff')
     list.should be_an_instance_of List
   end
 
   it 'returns the name of a list when given its id' do
-    list = List.new('Epicodus stuff', 1)
+    list = List.new('Epicodus stuff')
     list.save
     List.find(list.id).should eq list
   end
 
   it 'returns the tasks for a given list' do
-    list = List.new('Epicodus stuff', 1)
-    task1 = Task.new('stuff', 1)
-    task2 = Task.new('more stuff', 1)
+    list = List.new('Epicodus stuff')
+    list.save
+    task1 = Task.new('stuff', list.id)
+    task2 = Task.new('more stuff', list.id)
     task1.save
     task2.save
-    list.tasks.should eq [task1, task2]
+    puts list.id
+    List.all[0].tasks.should eq [task1, task2]
   end
 end
--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -5,18 +5,18 @@ require 'spec_helper'
 
 describe Task do
   it 'is initialized with a name' do
-    task = Task.new('learn SQL', 1)
+    task = Task.new('learn SQL', 0)
     task.should be_an_instance_of Task
   end
 
   it 'tells you its name' do
-    task = Task.new('learn SQL', 1)
+    task = Task.new('learn SQL', 0)
     task.name.should eq 'learn SQL'
   end
 
   it 'tells you its list ID' do
-    task = Task.new('learn SQL', 1)
-    task.list_id.should eq 1
+    task = Task.new('learn SQL', 0)
+    task.list_id.should eq 0
   end
 
   it 'starts off with no tasks' do
@@ -24,20 +24,20 @@ describe Task do
   end
 
   it 'lets you save tasks to the database' do
-    task = Task.new('learn SQL', 1)
+    task = Task.new('learn SQL', 0)
     task.save
     Task.all.should eq [task]
   end
 
   it 'is the same task if it has the same name and ID' do
-    task1 = Task.new('learn SQL', 1)
-    task2 = Task.new('learn SQL', 1)
+    task1 = Task.new('learn SQL', 0)
+    task2 = Task.new('learn SQL', 0)
     task1.should eq task2
   end
 
   it 'deletes a task when the name is entered' do
-    task1 = Task.new('learn SQL', 1)
-    task2 = Task.new('sweep floors', 2)
+    task1 = Task.new('learn SQL', 0)
+    task2 = Task.new('sweep floors', 0)
     task1.save
     task2.save
     task1.delete_task(task1.name)
